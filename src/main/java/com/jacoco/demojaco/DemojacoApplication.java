13d9a8645a678d9d7de122c4624c1fa1b4aa1bff
ambivam <rajanikanth.bathula@gmail.com>
2021-11-24 20:04:59 +0530

    made changes using jacoco

---
+++ src/main/java/com/jacoco/demojaco/DemojacoApplication.java
@@ -0,0 +1,13 @@
+package com.jacoco.demojaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojacoApplication.class, args);
+	}
+
+}
